fb8df333ab8d69c5d6a4b190fa13b0398da84209
SrihariWebDeveloper <146625393+SrihariWebDeveloper@users.noreply.github.com>
2024-08-04 20:00:51 +0530

    Update context.jsx

--- forntend/src/components/context/context.jsx
+++ forntend/src/components/context/context.jsx
@@ -4,7 +4,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) =>{
 
-    const url = 'http://localhost:4000';
+    const url = 'http://localhost:10000';
 
 
     const ContextValue ={
