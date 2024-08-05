20aa4df2289c08f5a1bb644004999328c1054559
SrihariWebDeveloper <146625393+SrihariWebDeveloper@users.noreply.github.com>
2024-08-05 12:05:15 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url ='https://medical-backend-x262.onrender.com';
+  const url = "https://medical-backend-4sb8.onrender.com";
   return (
     <div>
       <ToastContainer/>
