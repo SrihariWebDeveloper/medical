992880f88abb5670856fbdf161a34c0468bfe366
Srihari <runnersunny07@gmail.com>
2024-08-07 15:54:36 +0530

    my-second-commit

--- backend/confic/db.js
+++ backend/confic/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
 
 export const dBConnection = async() =>{
-    await mongoose.connect('mongodb://localhost:27017/medical').then(()=>console.log("DB connected"))
+    await mongoose.connect('mongodb+srv://srihariDev:Srihari07@cluster0.rmhhhrk.mongodb.net/medical').then(()=>console.log("DB connected"))
 }
